0c8048082b18140193e80d6046b9d2a9036c07ef
Jarek Radosz <jarek@cvx.dev>
2026-01-26 20:07:40 +0100

    DEV: Update linting and add types

--- spec/components/topic_query_spec.rb
+++ spec/components/topic_query_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "rails_helper"
-
 describe TopicQuery do
   context "with suppress categories from latest configured" do
     let(:category) { Fabricate(:category) }
